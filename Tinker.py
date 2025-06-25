1c48c36181b1528a905cf61a3bc7046218cb1ba6
jivepanda <smacleod9494@gmail.com>
2025-06-25 21:01:35 +0800

    Added a box

--- Tinker.py
+++ Tinker.py
@@ -3,9 +3,16 @@ import tkinter as tk
 #creating game window
 window = tk.Tk()
 window.geometry("300x300")
-window.title('Todos')
+window.title('Word game')
 window['background'] = "#301934"
 greeting = tk.Label(text="Wonderful Word Game ",fg="white", bg="#301934")
 greeting.pack(pady=10)
+label = tk.Label(text="Welcome to Wonderful Word of the day, Guess a Word ",fg="white", bg="#301934")
+label.pack(pady=10)
+entry = tk.Entry()
+entry.pack(pady=10)
+
+GUESS = entry.get()
 #button = tk.Button( text='Click Me', width=25,height=5, fg='FFC0CB',bg='#FFFFFF')
 
+window.mainloop()
